63069ada35a33d7237b3118a4dad14902468a52d
backdawn <bogdan.izotkov@gmail.com>
2021-01-10 00:05:47 +0200

    home work 3

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,9 @@ function show_hide_password(target){
 	var input = document.getElementById('password-input');
 	var img = document.getElementById('image');
 	if (input.getAttribute('type') == 'password') {
-		target.classList.add('view');
 		input.setAttribute('type', 'text');
 		img.src='img/eye-open.png'
 	} else {
-		target.classList.remove('view');
 		input.setAttribute('type', 'password');
 		img.src='img/eye-close.png'
 	}
